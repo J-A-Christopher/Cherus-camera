f02355ba5c22553f7ef4ced8325511405be862a9
Cj <christopherjesse45@gmail.com>
2023-04-15 13:49:51 +0300

    Added multiple image fetching

--- lib/models/data_retrieval_model.dart
+++ lib/models/data_retrieval_model.dart
@@ -1,23 +1,33 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
 class FireStoreDataBase {
-  String? downloadUrl;
-
-  Future getData() async {
-    try {
-      await downloadUrlExample();
-      return downloadUrl;
-    } catch (e) {
-      print(e);
-      return null;
-    }
+  Future<List<String>> getImageUrls(String folderName) async {
+    final FirebaseStorage storage = FirebaseStorage.instance;
+    final ref = storage.ref().child(folderName);
+    ListResult result = await ref.listAll();
+    List<Future<String>> futures = [];
+    result.items.forEach((Reference ref) {
+      futures.add(ref.getDownloadURL());
+    });
+    return await Future.wait(futures);
   }
+  // String? downloadUrl;
 
-  Future<void> downloadUrlExample() async {
-    downloadUrl = await FirebaseStorage.instance
-        .ref()
-        .child('images/Home.png')
-        .getDownloadURL();
-    print(downloadUrl.toString());
-  }
+  // Future getData() async {
+  //   try {
+  //     await downloadUrlExample();
+  //     return downloadUrl;
+  //   } catch (e) {
+  //     print(e);
+  //     return null;
+  //   }
+  // }
+
+  // Future<void> downloadUrlExample() async {
+  //   downloadUrl = await FirebaseStorage.instance
+  //       .ref()
+  //       .child('images/Home.png')
+  //       .getDownloadURL();
+  //   print(downloadUrl.toString());
+  // }
 }
--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -12,51 +12,71 @@ class _ImageDisplayState extends State<ImageDisplay> {
   bool almStatus = false;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SwitchListTile(
-            value: almStatus,
-            onChanged: (bool value) {
-              setState(() {
-                almStatus = value;
-              });
-            },
-            title: const Text('Toggle Alarm Status'),
-          ),
-          FutureBuilder(
-              future: FireStoreDataBase().getData(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Text('Something went Wrong');
-                }
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return Container(
-                      margin: const EdgeInsets.only(top: 80),
-                      height: 400,
-                      width: 400,
-                      child: Image.network(snapshot.data.toString()));
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              })
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SwitchListTile(
+          value: almStatus,
+          onChanged: (bool value) {
+            setState(() {
+              almStatus = value;
+            });
+          },
+          title: const Text('Toggle Alarm Status'),
+        ),
+        FutureBuilder(
+          future: FireStoreDataBase().getImageUrls('images'),
+          builder: (context, AsyncSnapshot<List<String>> snapshot) {
+            if (snapshot.hasData) {
+              List<String> urls = snapshot.data!;
+              return Expanded(
+                child: ListView.builder(
+                  itemBuilder: ((context, index) {
+                    return Image.network(urls[index]);
+                  }),
+                  itemCount: urls.length,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error:${snapshot.error}');
+            }
 
-          // Center(
-          //   child: SizedBox(
-          //     height: 200,
-          //     width: 200,
-          //     child: Image.asset('assets/internet.png'),
-          //   ),
-          // ),
-          // const SizedBox(
-          //   height: 20,
-          // ),
-          // ElevatedButton(
-          //     onPressed: () {}, child: const Text('Get Latest Image !'))
-        ],
-      ),
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+
+          // FireStoreDataBase().getData(),
+          // builder: (context, snapshot) {
+          //   if (snapshot.hasError) {
+          //     return const Text('Something went Wrong');
+          //   }
+          //   if (snapshot.connectionState == ConnectionState.done) {
+          //     return Container(
+          //         margin: const EdgeInsets.only(top: 80),
+          //         height: 400,
+          //         width: 400,
+          //         child: Image.network(snapshot.data.toString()));
+          //   }
+          //   return const Center(
+          //     child: CircularProgressIndicator(),
+          //   );
+          // }
+        )
+
+        // Center(
+        //   child: SizedBox(
+        //     height: 200,
+        //     width: 200,
+        //     child: Image.asset('assets/internet.png'),
+        //   ),
+        // ),
+        // const SizedBox(
+        //   height: 20,
+        // ),
+        // ElevatedButton(
+        //     onPressed: () {}, child: const Text('Get Latest Image !'))
+      ],
     );
   }
 }
